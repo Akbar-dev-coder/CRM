10dd1abad9b5aaa5cddea2ab793e39aaa9443caa
Md Akbar <mdakbarali264@gmail.com>
2025-06-09 16:44:16 +0530

    add file

---
+++ backend/src/settings/useMoney.js
@@ -0,0 +1,63 @@
+const currency = require('currency.js');
+
+const useMoney = ({ settings }) => {
+  const {
+    currency_symbol,
+    currency_position,
+    decimal_sep,
+    thousand_sep,
+    cent_precision,
+    zero_format,
+  } = settings;
+
+  function currencyFormat(amount) {
+    const value = currency(amount, {
+      separator: thousand_sep,
+      decimal: decimal_sep,
+      symbol: ' ',
+      precision: cent_precision,
+    });
+
+    return value.value === 0 && zero_format ? '0.00' : value.format();
+  }
+
+  // function currencyFormat(amount) {
+  //   return currency(amount).dollar() > 0 || !zero_format
+  //     ? currency(amount, {
+  //         separator: thousand_sep,
+  //         decimal: decimal_sep,
+  //         symbol: '',
+  //         precision: cent_precision,
+  //       }).format()
+  //     : 0 +
+  //         currency(amount, {
+  //           separator: thousand_sep,
+  //           decimal: decimal_sep,
+  //           symbol: '',
+  //           precision: cent_precision,
+  //         }).format();
+  // }
+
+  function moneyFormatter({ amount = 0 }) {
+    return currency_position === 'before'
+      ? currency_symbol + ' ' + currencyFormat(amount)
+      : currencyFormat(amount) + ' ' + currency_symbol;
+  }
+
+  function amountFormatter({ amount = 0 }) {
+    return currencyFormat(amount);
+  }
+
+  return {
+    moneyFormatter,
+    amountFormatter,
+    currency_symbol,
+    currency_position,
+    decimal_sep,
+    thousand_sep,
+    cent_precision,
+    zero_format,
+  };
+};
+
+module.exports = useMoney;
---
+++ frontend/src/pages/Customer/config.js
@@ -0,0 +1,21 @@
+export const fields = {
+  name: {
+    type: 'string',
+  },
+  country: {
+    type: 'country',
+    // color: 'red',
+  },
+  address: {
+    type: 'string',
+  },
+  gstin: {
+    type: 'string',
+  },
+  phone: {
+    type: 'phone',
+  },
+  email: {
+    type: 'email',
+  },
+};
