e2983d6216001d67901bf9feb7492a01c60b7fc9
Md Akbar <mdakbarali264@gmail.com>
2025-06-25 16:34:09 +0530

    add download btn

--- frontend/src/components/DataTable/DataTable.jsx
+++ frontend/src/components/DataTable/DataTable.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect } from 'react';
-
+import * as XLSX from 'xlsx';
 import {
   EyeOutlined,
   EditOutlined,
@@ -8,10 +8,12 @@ import {
   RedoOutlined,
   ArrowRightOutlined,
   ArrowLeftOutlined,
+  DownloadOutlined,
 } from '@ant-design/icons';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 import { Dropdown, Table, Button, Input } from 'antd';
 import { PageHeader } from '@ant-design/pro-layout';
-
 import { useSelector, useDispatch } from 'react-redux';
 import { crud } from '@/redux/crud/actions';
 import { selectListItems } from '@/redux/crud/selectors';
@@ -46,6 +48,82 @@ export default function DataTable({ config, extra = [] }) {
   const translate = useLanguage();
   const { moneyFormatter } = useMoney();
   const { dateFormat } = useDate();
+
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  const handleDownloadExcel = () => {
+    const exportColumn = dataTableColumns.filter((col) => col.dataIndex);
+    const exportkey = exportColumn.map((col) => col.dataIndex);
+    const headerMap = exportColumn.reduce((acc, col) => {
+      acc[col.dataIndex] = typeof col.title === 'string' ? col.title : col.dataIndex;
+      return acc;
+    }, {});
+
+    const cleanData = dataSource.map((row) => {
+      const filterRow = {};
+      exportkey.forEach((key) => {
+        filterRow[headerMap[key]] = row[key];
+      });
+      return filterRow;
+    });
+
+    const worksheet = XLSX.utils.json_to_sheet(cleanData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, `${entity}`);
+    XLSX.writeFile(workbook, `${capitalize(entity)}.xlsx`);
+  };
+
+  const handleDownloadPDF = () => {
+    const doc = new jsPDF();
+    const exportColumns = dataTableColumns.filter((col) => col.dataIndex);
+    const exportKey = exportColumns.map((col) => col.dataIndex);
+    const headerMap = exportColumns.reduce((acc, col) => {
+      acc[col.dataIndex] = typeof col.title === 'string' ? col.title : col.dataIndex;
+      return acc;
+    }, {});
+
+    const tableColumns = exportKey.map((key) => headerMap[key]);
+    const tableRows = dataSource.map((row) => exportKey.map((key) => row[key]));
+
+    doc.setFontSize(16);
+    doc.setTextColor('#333333');
+    doc.text(`${entity.charAt(0).toUpperCase() + entity.slice(1)} List`, 14, 20);
+
+    autoTable(doc, {
+      startY: 25,
+      head: [tableColumns],
+      body: tableRows,
+      theme: 'striped',
+      headStyles: {
+        fillColor: [22, 160, 133],
+        textColor: 255,
+        fontStyle: 'bold',
+        halign: 'left',
+        fontSize: 5,
+      },
+      bodyStyles: {
+        fontSize: 5,
+        valign: 'middle',
+        halign: 'left',
+      },
+      styles: {
+        overflow: 'linebreak',
+        minCellWidth: 'auto',
+      },
+      didDrawPage: function (data) {
+        const dateStr = new Date().toLocaleString();
+        doc.setFontSize(8);
+        doc.setTextColor('#888');
+        doc.text(
+          `Generated on: ${dateStr}`,
+          data.settings.margin.left,
+          doc.internal.pageSize.height - 10
+        );
+      },
+    });
+
+    doc.save(`${capitalize(entity)}.pdf`);
+  };
 
   const items = [
     {
@@ -194,6 +272,34 @@ export default function DataTable({ config, extra = [] }) {
           </Button>,
 
           <AddNewItem key={`${uniqueId()}`} config={config} />,
+          ['client', 'vender'].includes(entity) && (
+            <Dropdown
+              key={'downloadDropdown'}
+              menu={{
+                items: [
+                  {
+                    key: 'excel',
+                    label: 'Download as Excel',
+                    icon: <DownloadOutlined />,
+                  },
+                  {
+                    key: 'pdf',
+                    label: 'Download as PDF',
+                    icon: <DownloadOutlined />,
+                  },
+                ],
+                onClick: ({ key }) => {
+                  if (key === 'excel') {
+                    handleDownloadExcel();
+                  } else if (key === 'pdf') {
+                    handleDownloadPDF();
+                  }
+                },
+              }}
+            >
+              <Button icon={<DownloadOutlined />}>{translate('download')}</Button>
+            </Dropdown>
+          ),
         ]}
         style={{
           padding: '20px 0px',
